7e1c3c778bd961c9d90e29d3b2b3804bf325bb72
Derek Kennard <dkennard@homeinfusion.com>
2021-06-05 14:22:29 -0400

    Add project files.

---
+++ CuisineType.cs
@@ -0,0 +1,18 @@
+﻿// Created by Derek Kennard
+// Solution: OdeToFood
+// Project Name: OdeToFood.Core
+// File Name: CuisineType.cs
+// Created on: 05/25/2021 at 12:29 AM
+// Edited on: 05/25/2021 at 12:30 AM
+// Developed and Copyrighted by Derek "Doctork" Kennard
+
+namespace OdeToFood.Core
+{
+    public enum CuisineType
+    {
+        None,
+        Mexican,
+        Italian,
+        Indian
+    }
+}
---
+++ Restaurant.cs
@@ -0,0 +1,18 @@
+﻿// Created by Derek Kennard
+// Solution: OdeToFood
+// Project Name: OdeToFood.Core
+// File Name: Restaurant.cs
+// Created on: 05/25/2021 at 12:27 AM
+// Edited on: 05/25/2021 at 12:30 AM
+// Developed and Copyrighted by Derek "Doctork" Kennard
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
